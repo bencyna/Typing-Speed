5cbe2680c26f4cff7b035945c6179db06d1ccc94
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-12 18:40:42 +1030

    make screena bit nicer before I add some cool stuff

--- screen_setup.py
+++ screen_setup.py
@@ -1,23 +1,25 @@
 from tkinter import *
+
+
 # from word import Words
 
 
 class SetupScreen():
     def __init__(self, window):
         self.window = window
-        self.window.config(padx=50, pady=50)
+        self.window.config(padx=80, pady=80, background="#7FDBFF")
         self.window.title("Typing speed test")
-        canvas = Canvas(width=200, height=200, highlightthickness=0)
+        canvas = Canvas(width=200, height=200, background="#7FDBFF", highlightthickness=0)
         canvas.grid(row=0, column=1)
         self.add_items_to_scren()
 
     def add_items_to_scren(self):
         # Add title
-        website_label = Label(text="Typing test: ", font=("Arial", 20), justify='left')
-        website_label.grid(column=1, row=0)
+        website_label = Label(text="Typing test: ", background="#7FDBFF", font=("Arial", 20), justify='left')
+        website_label.grid(column=0, row=0)
 
         # Start list
-        words_list = Label(text="one, two, three, words")
+        words_list = Label(text="one, two, three, words", background="#7FDBFF")
         words_list.grid(column=1, row=1)
 
         # title for input
